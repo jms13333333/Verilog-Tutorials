f0aca18585e852d5cbfb31232c75f7feb488145f
jms13333333 <157331327+jms13333333@users.noreply.github.com>
2024-06-02 10:52:38 +0800

    Create 0.The Modules.v
    A module provides a template from which you can create actual objects. When a module is invoked, Verilog creates a unique object from this template

---
+++ 0.The Modules.v
@@ -0,0 +1,10 @@
+//Defining the circuit modules
+module VerilogTutorials00(
+  //Define the I/O ports
+  input a,
+  output b
+);
+  //Assigning the wires 
+  assign b = a; //Assign output = input
+  //The end of this circuit block
+endmodule
